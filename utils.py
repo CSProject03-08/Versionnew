8df023c0e9ac25c39ead13922531c6513c86bd80
Mo777-arch <m.k4sper@proton.me>
2025-12-08 20:40:30 +0100

    button

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def logout():
     Returns:
         None
     """
-    if st.button("Logout"):
+    if st.button("Logout", key="redirect"):
         # deletes data related to session states
         for key in ["user_ID", "role", "username"]:
             if key in st.session_state:
